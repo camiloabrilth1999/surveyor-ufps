703848dd04d6cdc3ac154723c557a2a0db52e2c4
camiloandresoa99 <camilo.obando@ccoins.io>
2020-05-28 23:34:29 -0500

    config boostrap

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :load_user
+
+  def show
+  end
+
+  private
+
+  def load_user
+    @user = User.find_by(username: params[:username])
+  end
+end
