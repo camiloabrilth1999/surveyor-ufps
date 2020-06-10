c6538af0b9bd2f842298cb48520e8945d3e6d2b7
camiloandresoa99 <camilo.obando@ccoins.io>
2020-06-09 20:18:13 -0500

    modify seed for users

--- db/seeds/users.rb
+++ db/seeds/users.rb
@@ -1,10 +1,12 @@
-User.create(email: "admin@admin.com", password: "123123123", name: "Admin",
-  lastname: "Admin", code: "1151493admin", identification_type_id: 1, document_number: "1090528761admin",
-  contact_number: "31896996123admin")
+# frozen_string_literal: true
 
-  User.create(email: "camiloandresoa@ufps.edu.co", password: "123123123", name: "Camilo Andres",
-    lastname: "Obando Abril", code: "1151493", identification_type_id: 1, document_number: "1090528761",
-    contact_number: "31896996123", academic_program_id: 4)
+User.create(email: 'admin@admin.com', password: '123123123', name: 'Admin',
+            lastname: 'Admin', code: '1151493', identification_type_id: 1, document_number: '1090528761',
+            contact_number: '3001111111')
+
+User.create(email: 'admin2@admin2.com', password: 'admin123', name: 'Admin2',
+            lastname: 'Admin2', code: '1234567', identification_type_id: 1, document_number: '1234567890',
+            contact_number: '3110001111')
 
 User.first.add_role :director_de_comite
-User.second.add_role :director_de_programa
+User.second.add_role :director_de_comite
