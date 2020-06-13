a0cff87eacbc480f71676d3ad0fedf004d8031ff
camiloandresoa99 <camilo.obando@ccoins.io>
2020-06-13 12:32:01 -0500

    modify passwords controller

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -2,34 +2,9 @@
 
 class Users::PasswordsController < Devise::PasswordsController
   layout "public"
-  # GET /resource/password/new
-  # def new
-  #   super
-  # end
+  protected
 
-  # POST /resource/password
-  # def create
-  #   super
-  # end
-
-  # GET /resource/password/edit?reset_password_token=abcdef
-  # def edit
-  #   super
-  # end
-
-  # PUT /resource/password
-  # def update
-  #   super
-  # end
-
-  # protected
-
-  # def after_resetting_password_path_for(resource)
-  #   super(resource)
-  # end
-
-  # The path used after sending reset password instructions
-  # def after_sending_reset_password_instructions_path_for(resource_name)
-  #   super(resource_name)
-  # end
+  def after_resetting_password_path_for(_resource)
+    unauthenticated_root_path
+  end
 end
